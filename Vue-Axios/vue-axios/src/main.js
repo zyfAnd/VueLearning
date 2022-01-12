19eee37bf368768a500da2db72f5cb94bc7ec2a1
yanfu <18842647603@163.com>
2022-01-12 22:43:24 +0800

    Vue Axios + ElementUI 生命周期函数 降低ElementUI 版本 显示 表格 <el-table-column

--- Vue-Axios/vue-axios/src/main.js
+++ Vue-Axios/vue-axios/src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(VueAxios, axios)
-Vue.use(ElementUI);
-Vue.config.productionTip = false
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(VueAxios, axios)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
--- Vue-ElementUI/vue-elementui/src/components/Stars.vue
+++ Vue-ElementUI/vue-elementui/src/components/Stars.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+<!--  <div>-->
 <!--    <ul id="v-for-object" >-->
 <!--      <li v-for="value in mylist" :key="value.name" >-->
 <!--        {{ value.id }} {{ value.name }} {{ value.number }}-->
@@ -11,22 +11,12 @@
         stripe
         style="width: 100%">
       <el-table-column
-          prop="id"
-          label="ID"
-          width="180">
-      </el-table-column>
-      <el-table-column
           prop="name"
-          label="姓名"
-          width="180">
-      </el-table-column>
-      <el-table-column
-          prop="number"
-          label="粉丝"
+          label="日期"
           width="180">
       </el-table-column>
     </el-table>
-  </div>
+<!--  </div>-->
 </template>
 
 <style>
@@ -36,7 +26,6 @@
 <script>
 export default {
   name: 'Stars',
-  //生命周期函数
   mounted(){
     this.axios.get('http://127.0.0.1:8011/ins/star').then((response) => {
       console.log(response.data)
