47333bb5c1255b65bd0bd286a84120ed7409eb0b
yanfu <18842647603@163.com>
2022-01-13 22:20:09 +0800

    Vue 事件处理

---
+++ Vue-事件处理/vue-event/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Stars></Stars>
+  </div>
+</template>
+
+<script>
+import Stars from './components/Stars.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Stars
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue-事件处理/vue-event/src/components/Stars.vue
@@ -0,0 +1,54 @@
+<template>
+  <div >
+    <el-button @click="updateList" type="primary">点我清空</el-button>
+    <hr>
+    <el-table
+        :data="mylist"
+        stripe
+        style="width: 100%">
+      <el-table-column
+          prop="id"
+          label="ID"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="number"
+          label="粉丝"
+          width="180">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stars',
+  data(){
+    return {
+      mylist: null
+    }
+  },
+  mounted() {
+    this.axios.get('http://127.0.0.1:8011/ins/star').then((response)=>{
+      this.mylist = response.data.list
+    })
+  },
+  methods : {
+    updateList (){
+      // this.axios.get('http://127.0.0.1:8011/ins/star').then((response) => {
+      //   this.myList = null
+      // })
+      this.mylist = null
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
