af4caf5599331a2b3dded77146a65e8dc4d840d5
yanfu <18842647603@163.com>
2022-01-18 22:31:51 +0800

    Vue 数据绑定

--- Vue-脚手架/vue-cli/src/main.js
+++ Vue-脚手架/vue-cli/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import axios from 'axios';
+import VueAxios from 'vue-axios'
+import 'element-ui/lib/theme-chalk/index.css';
+import VueAxios from "vue-axios";
+Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
