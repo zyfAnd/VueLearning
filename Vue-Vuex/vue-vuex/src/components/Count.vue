b8ebd757a5d357d03f94e9d35e2fe40a6ade4623
yanfu <18842647603@163.com>
2022-01-22 18:13:14 +0800

    Vue vuex

--- Vue-Vuex/vue-vuex/src/components/Count.vue
+++ Vue-Vuex/vue-vuex/src/components/Count.vue
@@ -1,12 +1,17 @@
 <template>
 
   <div>
-    <select>
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-    </select>
-    <button @click=""></button>
+<!--    element selection 选择器-->
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+      </el-option>
+    </el-select>
+    <el-button @click="add" type="primary"> add </el-button>
+
   </div>
 
 </template>
@@ -14,14 +19,31 @@
 <script>
 export default {
   name: "Count",
-  data(){
+  data() {
     return {
-
-      sum: 0
+      options: [{
+        value: '1',
+        label: '1'
+      }, {
+        value: '2',
+        label: '2'
+      }, {
+        value: '3',
+        label: '3'
+      }, {
+        value: '4',
+        label: '4'
+      }, {
+        value: '5',
+        label: '5'
+      }],
+      value: ''
     }
   },
   methods:{
+    add() {
 
+    }
   }
 }
 </script>
--- Vue-Vuex/vue-vuex/src/main.js
+++ Vue-Vuex/vue-vuex/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// 引入 ElementUI UI 组件
 import ElementUI from 'element-ui'
+//引入 element ui 全部组件
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI)
 Vue.config.productionTip = false
