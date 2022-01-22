7a53263dc44905948a897ef63ce4f6a718f1913b
yanfu <18842647603@163.com>
2022-01-22 17:51:11 +0800

    Vue vuex

---
+++ Vue-Vuex/vue-vuex/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Count></Count>
+  </div>
+</template>
+
+<script>
+
+
+import Count from "./components/Count";
+export default {
+  name: 'App',
+  components: {
+    Count
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue-Vuex/vue-vuex/src/components/Count.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div>
+    <select>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click=""></button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Count",
+  data(){
+    return {
+
+      sum: 0
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
